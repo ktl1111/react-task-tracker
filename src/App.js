16e879218ba994d438185510581249a23bfcb0dd
Vany Liu <vany8462.fl00@gmail.com>
2022-04-24 21:43:39 +0800

    update add button

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { useState } from 'react'
 
 
 const App = () => {
+    const [showAddTask, setShowAddTask] = useState(false)
     const [tasks, setTasks] = useState(
 [
     {
@@ -30,7 +31,7 @@ const App = () => {
 ])
 
   // Add Task
-  const addTask = (task) => {
+    const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1
     const newTask = { id, ...task }
     setTasks([...tasks, newTask])
@@ -51,8 +52,8 @@ const App = () => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={ () => setShowAddTask(!showAddTask) } showAdd={showAddTask} />
+        {showAddTask && <AddTask onAdd={addTask} />}
         {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks To Show')}
     </div>
   );
